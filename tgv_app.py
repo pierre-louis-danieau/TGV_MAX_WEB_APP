1cc131748838b82bc9ec7764b180bc48110338ed
Pierre-Louis DANIEAU <67114372+pierre-louis-danieau@users.noreply.github.com>
2022-07-03 17:29:39 +0200

    Add files via upload

--- tgv_app.py
+++ tgv_app.py
@@ -5,35 +5,14 @@ from datetime import timedelta
 import pandas as pd
 import streamlit as st
 from PIL import Image
+from dateutil import parser
+import locale
+
+
 
 
 
 ############### MAIN Functions ########################
-
-def df_init():
-    url = 'https://ressources.data.sncf.com/api/records/1.0/search/?dataset=tgvmax&q=&rows=10000&sort=-date&facet=date&facet=origine&facet=destination&facet=od_happy_card&refine.origine=PARIS+(intramuros)&refine.destination=LA+ROCHELLE+VILLE'
-
-    response_API_tgv = requests.get(url)
-
-    data = response_API_tgv.text
-    parse_json = json.loads(data)
-
-
-    arr = []
-    for i in range(len(parse_json['records'])):
-        record = parse_json['records'][i]['fields']
-        date = record['date']
-        heure_arrivee = record['heure_arrivee']
-        heure_depart = record['heure_depart']
-        origine = record['origine']
-        destination = record['destination']
-
-        data_point = [origine, destination, date, heure_depart, heure_arrivee]
-        arr.append(data_point)
-
-    df = pd.DataFrame(arr, columns = ['origine', 'destination', 'date','heure_depart', 'heure_arrivee'])
-    return df
-
 
 
 def param(df_ville_origine,df_ville_destination):
@@ -50,58 +29,76 @@ def param(df_ville_origine,df_ville_destination):
           values_destination,
           index = list(values_destination).index('LA ROCHELLE VILLE'))
 
-
      today = datetime.now()
-     option_date = st.sidebar.date_input(
+     placeholder = st.sidebar.empty()
+     option_date = placeholder.date_input(
           "Date de départ",
           today,
           min_value = today,
           max_value = today + timedelta(days=29)
           )
+          
+     all_dates = st.sidebar.checkbox('Toutes les places dispo sur 30 jours')
+
+     if all_dates == True:
+          placeholder.empty()
 
      bouton_launch_search=st.sidebar.button("Rechercher")
      
-     st.sidebar.write(" ")
-     st.sidebar.write(" ")
-     st.sidebar.write(" ")
-     st.sidebar.write(" ")
-     st.sidebar.write(" ")
-     st.sidebar.write(" ")
-     st.sidebar.write(" ")
-     st.sidebar.write(" ")
+
      st.sidebar.write(" ")
      st.sidebar.write(" ")
      st.sidebar.write("Me contacter [Linkedin](https://www.linkedin.com/in/pierre-louis-danieau/)")
-     st.sidebar.write(" ")
      st.sidebar.write("**©️ Copyright 2022 - Pierre-Louis Danieau**")
 
-     return option_origine,option_destination,option_date,bouton_launch_search
+     return option_origine,option_destination,option_date,all_dates,bouton_launch_search
 
-def dataframe_train(option_origine,option_destination,option_date):
+@st.cache
+def mis_a_jour():
+     locale.setlocale(locale.LC_TIME,'')
+     url = "https://ressources.data.sncf.com/api/records/1.0/search/?dataset=tgvmax&q=&rows=1&sort=-date&facet=date&facet=origine&facet=destination&facet=od_happy_card"
+     response_API_tgv = requests.get(url)
+     data = response_API_tgv.text
+     parse_json = json.loads(data)
+     mis_a_jour= parse_json['records'][0]['record_timestamp']
+     mis_a_jour_date = parser.parse(mis_a_jour)
+     str_maj = mis_a_jour_date.strftime("%A %d %B %Y à %H:%M")
+     return str_maj
+
+def dataframe_train(option_origine,option_destination,option_date,all_dates):
      day = option_date.day
      month = option_date.month
      year = option_date.year
 
-     url = 'https://ressources.data.sncf.com/api/records/1.0/search/?dataset=tgvmax&q=&rows=10000&sort=-date&facet=date&facet=origine&facet=destination&facet=od_happy_card&refine.origine={}&refine.date={}%2F{}%2F{}&refine.destination={}'.format(option_origine,year,month,day,option_destination)
+     if all_dates == False:
+          # Specific dates
+          url = 'https://ressources.data.sncf.com/api/records/1.0/search/?dataset=tgvmax&q=&rows=10000&sort=-date&facet=date&facet=origine&facet=destination&facet=od_happy_card&refine.origine={}&refine.date={}%2F{}%2F{}&refine.destination={}'.format(option_origine,year,month,day,option_destination)
+     else :
+          # All dates
+          url = 'https://ressources.data.sncf.com/api/records/1.0/search/?dataset=tgvmax&q=&rows=10000&sort=-date&facet=date&facet=origine&facet=destination&facet=od_happy_card&refine.origine={}&refine.destination={}&refine.od_happy_card=OUI'.format(option_origine,option_destination)
+     
      response_API_tgv = requests.get(url)
-
      data = response_API_tgv.text
      parse_json = json.loads(data)
-
 
      arr = []
      for i in range(len(parse_json['records'])):
           record = parse_json['records'][i]['fields']
           date = record['date']
-          heure_arrivee = record['heure_arrivee']
-          heure_depart = record['heure_depart']
+          heure_arrivee = datetime.strptime(record['heure_arrivee'], '%H:%M')
+          heure_depart = datetime.strptime(record['heure_depart'], '%H:%M')
           origine = record['origine']
           destination = record['destination']
-
-          data_point = [origine, destination, date, heure_depart, heure_arrivee]
+          tgv_jeune_senior = (record['od_happy_card'])
+          data_point = [origine, destination, date, heure_depart, heure_arrivee, tgv_jeune_senior]
           arr.append(data_point)
-
-     df = pd.DataFrame(arr, columns = ['origine', 'destination', 'date','heure_depart', 'heure_arrivee'])
+     
+     df = pd.DataFrame(arr, columns = ['origine', 'destination', 'date','heure_depart', 'heure_arrivee','places disponibles'])
+     df = df.sort_values(by=['date','heure_depart'])
+     df['heure_arrivee'] = df['heure_arrivee'].apply(lambda x : x.strftime("%H:%M")+4*' ')
+     df['heure_depart'] = df['heure_depart'].apply(lambda x : x.strftime("%H:%M")+4*' ')
+     df['places disponibles'] = df['places disponibles'].apply(lambda x: 'PEUT ÊTRE'+4*' ' if x =="NON" else 'OUI'+10*' ')
+     df = df.reset_index(drop=True)
      return df
 
 
@@ -109,6 +106,12 @@ def formulaire():
     html='<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSexZO8N0YxIQN0wKRdgyQSsI0Q-0xGk0a3A-aXpZBApBGdfqg/viewform?embedded=true" width="640" height="499" frameborder="0" marginheight="0" marginwidth="0">Chargement…</iframe>'
     st.markdown(html, unsafe_allow_html=True)
 
+def color_df(val):
+     if val =="OUI"+10*' ':
+          color = '#8DEF7B'
+     else :
+          color = '#FC9F60'
+     return f'background-color: {color}'
 
 
 
@@ -116,102 +119,101 @@ def formulaire():
 
 
 if __name__ == "__main__":
+     locale.setlocale(locale.LC_TIME,'')
+     st.set_page_config(
+     page_title="Maxplorateur",
+     page_icon="🚅",
+     layout="wide",
+     initial_sidebar_state="expanded",
+     )
+
+     ## Hide menu
+     hide_menu_style = """
+        <style>
+        #MainMenu {visibility: hidden; }
+        footer {visibility: hidden;}
+        </style>
+        """
+     st.markdown(hide_menu_style, unsafe_allow_html=True)
+     ## End hide menu
+
+
      image = Image.open('photo_train.jpg')
      
      col1_first, col2_first = st.columns(2)
+     str_maj = mis_a_jour()
 
      with col1_first:
-          st.markdown("<h2 style='color: RoyalBlue;'>Maxplorateur App</h2>", unsafe_allow_html=True)
+          st.markdown("<h2 style='color: RoyalBlue;'>Maxplorateur App - Recherche</h2>", unsafe_allow_html=True)
           st.write("""
-               **L'ensemble des trains éligibles à l'abonnement TGV max en un seul endroit !**
+               **L'ensemble des trains éligibles à l'abonnement TGV max !**
                
                Définissez vos critères dans la barre de gauche !
                """)
+          st.markdown("<p style='color: RoyalBlue;'>Mise à jour des trains le : {} </p>".format(str_maj), unsafe_allow_html=True)
+
 
           
      with col2_first:
-          st.image(image, caption='Photo by Ankush Minda', use_column_width='auto')
+          st.image(image, caption='Photo by Ankush Minda', width = 350)
      
      st.write(' ')
      col1, col2, col3 = st.columns(3)
 
 
-     df_ville_origine = pd.read_csv('ville_origine.csv')
-     df_ville_destination = pd.read_csv('ville_destination.csv')
+     df_ville_origine = pd.read_csv('/Users/pierre-louis.danieau/Documents/perso/TGV_max_project/ville_origine.csv')
+     df_ville_destination = pd.read_csv('/Users/pierre-louis.danieau/Documents/perso/TGV_max_project/ville_destination.csv')
      del df_ville_origine['Unnamed: 0']
      del df_ville_destination['Unnamed: 0']
 
+     st.sidebar.markdown("# 1️⃣ - Recherche de TGV Max")
 
-     st.sidebar.markdown("<h2 style='text-align: center; color: RoyalBlue;'>Critères</h2>", unsafe_allow_html=True)
 
-
-     option_origine,option_destination,option_date,bouton_launch_search = param(df_ville_origine,df_ville_destination)
+     option_origine,option_destination,option_date,all_dates,bouton_launch_search = param(df_ville_origine,df_ville_destination)
 
 
      if bouton_launch_search == True:
-          df_train_filter = dataframe_train(option_origine,option_destination,option_date)
+          df_train_filter = dataframe_train(option_origine,option_destination,option_date,all_dates)
           if df_train_filter.shape[0] == 0:
                st.write(' ')
                st.markdown("<h4 style='text-align: center; color: RoyalBlue;'>Mince..  🙁 </h4>", unsafe_allow_html=True)
-               st.markdown("<h4 style='text-align: center; color: RoyalBlue;'>Aucun trains de prévus en TGV max pour l'instant avec ces critères.</h4>", unsafe_allow_html=True)
+               st.markdown("<h4 style='text-align: center; color: RoyalBlue;'>Aucun trains éligibles à TGV max pour l'instant avec ces critères.</h4>", unsafe_allow_html=True)
                st.markdown("<h6 style='text-align: center; color: RoyalBlue;'>Réessaye demain vers 9h-10h ! .</h6>", unsafe_allow_html=True)
                st.write(' ')
                st.write(' ')
-               with st.expander("🚆 Informations importantes sur les trains"):
-                    st.write("""
-                    ⚠️ Les trains affichés sont les trains **élligibles** à l'abonnement TGV Max. 
-
-                    Cependant, il arrive (de temps en temps) que le prix du billet ne soit tout de même pas à 0 € sur le site de la SNCF. 
-
-                    En effet, lorsque la demande est trop élevée, la SNCF fait payer les places alors même que le train est éligible à TGV Max.
-
-                    De plus les données de l'application sont mise à jour tous les matins vers 9h. Si vous vous connectez trop tardivement dans la journée, il se peut que les places ne soient plus disponibles.
-
-                    Je suis actuellement en train de résoudre ces problèmes pour : 
-
-                         - Afficher uniquement les places à 0 €
-                    
-                         - Mise à jour plusieurs fois par jours des trains.
-                    
-                         - Mise en place d'une alerte par mail lorsqu'un une place est disponible.
-                    
-                    Partager ou parler autour de vous de cette application me permettra de gagner en visibilité auprès de la SNCF afin d'obtenir davantage de données sur les trains TGV Max et ainsi améliorer l'app !
-                    
-                    Merci beaucoup 🙂
-                    """)
-               with st.expander("📧 Recevez une alerte par mail lorsque qu'une place se libère !"):
-                    formulaire()
 
           else:
-               st.markdown("<h4 style='color: RoyalBlue;'>Voici la liste des trains dispos :  🎉🎉🎉</h4>", unsafe_allow_html=True)
-               df_train_filter.columns = ["Origine", "Destination", "Date", "Heure de départ", "Heure d'arrivée"]
-               st.dataframe(df_train_filter, 1000)
+               st.markdown("<h4 style='color: RoyalBlue;'>Voici la liste des trains éligibles :  🎉🎉🎉</h4>", unsafe_allow_html=True)
+               df_train_filter.columns = ["Origine", "Destination", "Date", "Heure de départ", "Heure d'arrivée", "Places disponibles"]
+               st.dataframe(df_train_filter.style.applymap(color_df, subset=[df_train_filter.columns[-1]]),3000)
                st.write(' ')
-               with st.expander("🚆 Informations importantes sur les trains"):
-                    st.write("""
-                    ⚠️ Les trains affichés sont les trains **élligibles** à l'abonnement TGV Max. 
+          
+          with st.expander("🚆 Informations importantes sur les trains"):
+               st.write("""
+               ⚠️ Les trains affichés sont les trains **élligibles** à l'abonnement TGV Max. 
 
-                    Cependant, il arrive (de temps en temps) que le prix du billet ne soit tout de même pas à 0 € sur le site de la SNCF. 
+               Or, tous les trains éligibles à TGV Max n'ont plus forcément de places diponibles à 0 €.
+          
+               C'est pourquoi tu retrouveras le champ "Places disponibles" dans la liste des trains proposés avec comme valeur :
 
-                    En effet, lorsque la demande est trop élevée, la SNCF fait payer les places alors même que le train est éligible à TGV Max.
+                    - "OUI", lorsqu'il reste des places disponibles en TGV Max à 0 €.
 
-                    De plus les données de l'application sont mise à jour tous les matins vers 9h. Si vous vous connectez trop tardivement dans la journée, il se peut que les places ne soient plus disponibles.
-
-                    Je suis actuellement en train de résoudre ces problèmes pour : 
-
-                         - Afficher uniquement les places à 0 €
+                    - "PEUT ETRE", lorsque je ne suis pas certain qu'il reste des places gratuites. 
                     
-                         - Mise à jour plusieurs fois par jours des trains.
-                    
-                         - Mise en place d'une alerte par mail lorsqu'un une place est disponible.
-                    
-                    Partager ou parler autour de vous de cette application me permettra de gagner en visibilité auprès de la SNCF afin d'obtenir davantage de données sur les trains TGV Max et ainsi améliorer l'app !
-                    
-                    Merci beaucoup 🙂
-                    """)
-               with st.expander("📧 Recevez une alerte par mail lorsque qu'une place se libère !"):
-                    formulaire()
+               En effet je n'ai malheureusement pas encore accès à toutes les données sur les TGV max de la SNCF, il y a donc des trains pour lesquels je ne sais pas si il reste des places dispos ou non.
+          
+               De plus, les données de l'application sont mises à jour une fois par jour en début de matinée. Et il se peut que la SNCF modifie plusieurs fois par jour l'éligibilité et / ou la disponibilité d'un train TGV Max. Il peut donc y avoir des inexactitudes entre ce que l'application renvoie et ce qui est proposé sur le site de la SNCF.  
+               
+               C'est pourquoi il vaut mieux se connecter le **matin** sur l'application Maxplorateur !
 
+               Mais pas de panique, je suis actuellement en train de résoudre ces problèmes !
+               
+               Partager ou parler autour de toi de cette application me permettra de gagner en visibilité auprès de la SNCF afin d'obtenir davantage de données sur les trains TGV Max et ainsi améliorer l'app Maxplorateur !
+               
+               Merci beaucoup 🙂
+               """)
+          with st.expander("📧 Recevez une alerte par mail lorsque qu'une place se libère !"):
+               formulaire()
 
 
           with col1:
@@ -222,4 +224,7 @@ if __name__ == "__main__":
                st.write(option_destination)
           with col3:
                st.subheader('Date')
-               st.write(option_date, ' : aaaa-mm-jj ')
+               if all_dates == True : 
+                    st.write('3O prochains jours')
+               else:
+                    st.write(option_date.strftime("%A %d %B %Y"))
